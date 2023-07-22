de402b4012da7c771c5d03c9198aeb8f00cedfad
mitaj <mitaj999@icloud.com>
2023-07-22 20:36:42 +0600

    Start first project for iOS

---
+++ MyIOSProject/MyIOSProject/MyIOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIOSProjectApp.swift
+//  MyIOSProject
+//
+//  Created by Дмитрий Митин on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyIOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
